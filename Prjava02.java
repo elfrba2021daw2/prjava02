18e771a793cf505bfae17b184ed501000566c1f0
elfrba2021daw2 <elfrba2021daw2@protonmail.com>
2020-12-15 20:02:53 +0100

    Treballant amb ordres Git i branques: Versió 1.1 del projecte prjava02

--- Prjava02.java
+++ Prjava02.java
@@ -17,10 +17,11 @@ public class Prjava02 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("versió 0.1 del projecte prjava02"); 	
+        System.out.println("versió 0.1 del projecte prjava02"); 
+		System.out.println("Creació d'una branca del projecte prjava02"); 	
         try {
-            InetAddress adreça = InetAddress.getLocalHost();
-            String hostname = adreça.getHostName();
+            InetAddress adreca = InetAddress.getLocalHost();
+            String hostname = adreca.getHostName();
             System.out.println("hostname="+hostname);
             System.out.println("Nom de l'usuari: " + System.getProperty("user.name"));
             System.out.println("Carpeta Personal: " + System.getProperty("user.home"));
